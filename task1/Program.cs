a55da69713ca2170b36c5982b70678ef4c7995ff
Viking <kmss13@mail.ru>
2023-06-22 19:49:52 +0400

    add all files

---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет,является ли оно палиндромом.
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int inputedNum = Convert.ToInt32(Console.ReadLine());
+    return inputedNum;
+}
+int number = ReadInt("Введите пятизначное целое число ");
+
+if ((number / 10000 == number % 10) && (number / 1000 % 10 == number / 10 % 10))
+{
+    Console.WriteLine($"{number} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"{number} не является палиндромом");
+}
+
+
+// Console.Write("Введите пятизначное число ");
+// string inputString = Console.ReadLine();
+
+// void  check_polindrom(string text)
+// {
+//     int len = inputString.Length;
+//     int count = 0;
+//     while (count < (len / 2))
+//     {
+//         if (inputString[count] == inputString[len - count - 1])
+//         {
+//             count++;
+//         }
+//         else 
+//         {
+//          Console.Write("Введенное число не палиндром");
+//         }
+//         Console.Write("Введенное число палиндром");
+   
+// }
+// check_polindrom(inputString);
---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+int ReadInt(string message)
+{
+    Console.Write($"{message} > ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите целое положительное число ");
+int result = 1;
+
+for (int i = 1; i <= number; i++)
+{
+    result = i * i * i;
+    Console.Write($"{result} ");
+}
